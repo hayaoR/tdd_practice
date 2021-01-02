d8337f5d514f8b14c127b1acac153e7b9de7deb1
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-02 13:19:20 +0900

    Moneyクラスに実装をpush upしてDollarクラスとFrancクラスを削除

--- src/com/company/Money.java
+++ src/com/company/Money.java
@@ -1,25 +1,34 @@
 package com.company;
 
-abstract class  Money {
+ public class Money {
     protected  int amount;
     protected  String currency;
 
+    Money(int amount, String  currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Money(amount, "USD");
     }
 
     static  Money franc(int amount) {
-        return new Franc(amount);
+        return new Money(amount, "CHF");
     }
 
-    abstract  Money times(int multiplier);
-    abstract  String currency();
+    Money times(int multiplier){
+        return new Money(amount*multiplier, currency);
+    }
+      String currency() {
+          return  currency;
+      }
 
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
         Money money = (Money) object;
-        return this.amount == money.amount;
+        return this.amount == money.amount && this.currency.equals(money.currency);
     }
 }
--- src/com/company/MoneyTest.java
+++ src/com/company/MoneyTest.java
@@ -2,8 +2,6 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
-import java.awt.event.MouseAdapter;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
@@ -11,12 +9,12 @@ class MoneyTest {
     @Test
     void multiplication() {
         Money five  = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(new Money(10, "USD"), five.times(2));
+        assertEquals(new Money(15, "USD"), five.times(3));
 
          five = Money.franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        assertEquals(new Money(10, "CHF"), five.times(2));
+        assertEquals(new Money(15, "CHF"), five.times(3));
     }
 
     @Test
@@ -27,13 +25,13 @@ class MoneyTest {
 
     @Test
     void equality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(6), new Dollar(5));
+        assertEquals(new Money(5, "USD"), new Money(5, "USD"));
+        assertNotEquals(new Money(5, "USD"), new Money(6, "USD"));
 
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(6), new Franc(5));
+        assertEquals(new Money(5, "CHF"), new Money(5, "CHF"));
+        assertNotEquals(new Money(5, "CHF"), new Money(6, "CHF"));
 
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertNotEquals(new Money(5, "USD"), new Money(5, "CHF"));
 
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
