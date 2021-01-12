ad07586a37d59d2f024038f90330392d247955e6
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-12 21:36:18 +0900

    Bankにrateを導入し通貨の為替を保存できるようにした

--- src/com/company/MoneyTest.java
+++ src/com/company/MoneyTest.java
@@ -57,6 +57,11 @@ class MoneyTest {
     }
 
     @Test
+    void identityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
+    @Test
     void currency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
